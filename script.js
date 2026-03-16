be2ce31879f6a127438480136cfc4f399b048e63
donato-glitch <eduardo.donato@neu.edu.ph>
2026-03-17 01:43:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+document.getElementById('visitorForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const name = document.getElementById('name').value;
+    const idNum = document.getElementById('idNum').value;
+    const purpose = document.getElementById('purpose').value;
+    const timeIn = new Date().toLocaleString();
 
+    const table = document.getElementById('logTable').getElementsByTagName('tbody')[0];
+    const newRow = table.insertRow();
+
+    newRow.innerHTML = `
+        <td>${name}</td>
+        <td>${idNum}</td>
+        <td>${purpose}</td>
+        <td>${timeIn}</td>
+    `;
+
+    document.getElementById('visitorForm').reset();
+});
